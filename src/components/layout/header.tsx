c168961984b2f6f975499c5723eee14621d5da16
Fran Bec <franbecvort@gmail.com>
2024-12-30 20:39:26 +0000

    blue

--- src/components/layout/header.tsx
+++ src/components/layout/header.tsx
@@ -12,7 +12,7 @@ import ModeToggle from "@/components/dark-mode/mode-toogle";
 
 const Header = () => {
   return (
-    <header className="flex h-16 shrink-0 items-center justify-between px-4 gap-2 transition-[width,height] ease-linear group-has-[[data-collapsible=icon]]/sidebar-wrapper:h-12">
+    <header className="flex h-16 shrink-0 items-center justify-between px-4 gap-2 transition-[width,height] ease-linear group-has-[[data-collapsible=icon]]/sidebar-wrapper:h-12 border-b-2">
       <div className="flex items-center gap-2">
         <SidebarTrigger className="-ml-1" />
         <Separator orientation="vertical" className="mr-2 h-4" />
